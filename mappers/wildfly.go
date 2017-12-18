4649a06a02b3627b46bc784566914ba459ff3e9a
Ataxexe <ataxexe@devnull.tools>
2017-12-18 16:04:55 -0200

    split the expression

--- mappers/wildfly.go
+++ mappers/wildfly.go
@@ -6,7 +6,9 @@ var WildflyMapper RegexpMapper
 
 func init() {
 	WildflyMapper = RegexpMapper{
-		Entry:      regexp.MustCompile(`(?P<time>(?P<date>\d{2,4}-\d{2}-\d{2,4}\s)?(\d{2}:\d{2}:\d{2},\d{3}))\s+\|?\s*(?P<level>\w+)\s+\|?\s*\[(?P<category>\S+)]\s+\|?\s*\((?P<origin>[^)]+)\)?\s?\|?\s?(?P<message>.+)`),
+		Entry: regexp.MustCompile(`(?P<time>(?P<date>\d{2,4}-\d{2}-\d{2,4}\s)?(\d{2}:\d{2}:\d{2},\d{3}))` +
+			`\s+\|?\s*(?P<level>\w+)\s+\|?\s*\[(?P<category>\S+)]\s+\|?\s*\((?P<origin>[^)]+)\)?\s?\|?\s?` +
+			`(?P<message>.+)`),
 		Exception:  JavaMapper.Exception,
 		Stacktrace: JavaMapper.Stacktrace,
 	}
