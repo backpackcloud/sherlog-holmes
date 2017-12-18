ae1af8a161f3699868276f72c101cab50dc7528b
Ataxexe <ataxexe@devnull.tools>
2017-12-18 16:04:41 -0200

    add a simple entry pattern

--- mappers/java.go
+++ mappers/java.go
@@ -6,10 +6,10 @@ var JavaMapper RegexpMapper
 
 func init() {
 	JavaMapper = RegexpMapper{
+		Entry:      regexp.MustCompile(`(?P<message>.+)`),
 		Exception:  regexp.MustCompile(`(?P<exception>\w+(\.\w+)+(Exception|Error|Fault))`),
 		Stacktrace: regexp.MustCompile(`^(\s+at)|(Caused by:)|(\s+\.{3}\s\d+\smore)`),
 	}
 	RegisteredMappers["base.java"] = JavaMapper
 	RegexpMappers["base.java"] = JavaMapper
 }
-
