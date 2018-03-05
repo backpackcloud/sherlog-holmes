572d384506dfd73ec9f3520b3b3f8cf4b6c9da68
Ataxexe <ataxexe@devnull.tools>
2018-03-05 18:54:23 -0300

    Refact test code

--- processors/count_test.go
+++ processors/count_test.go
@@ -3,84 +3,44 @@ package processors_test
 import (
 	"testing"
 
-	"bytes"
-
 	"github.com/devnull-tools/sherlog-holmes/domain"
-	"github.com/devnull-tools/sherlog-holmes/processors"
+	"github.com/devnull-tools/sherlog-holmes/test"
 )
-
-type CountTest struct {
-	T        *testing.T
-	Group    string
-	NewEntry func(attr string) *domain.Entry
-	values   map[string]int64
-}
-
-func (test *CountTest) expect(label string, count int64) {
-	if n := test.values[label]; n != count {
-		test.T.Errorf("Invalid count for %s. Expected: %d | Got: %d", label, count, n)
-	}
-}
-
-func (test *CountTest) doTest() {
-	var buffer bytes.Buffer
-	processor := processors.NewCountProcessor([]string{test.Group}, processors.Default, &buffer)
-
-	processor.Before()
-	processor.Execute(test.NewEntry("lorem"))
-	processor.Execute(test.NewEntry("bacon"))
-	processor.Execute(test.NewEntry("bacon"))
-	processor.Execute(test.NewEntry("lorem"))
-	processor.Execute(test.NewEntry("meh"))
-	processor.Execute(test.NewEntry("bacon"))
-	processor.Execute(test.NewEntry("meh"))
-	processor.Execute(test.NewEntry("lorem"))
-	processor.Execute(test.NewEntry("bacon"))
-	processor.After()
-
-	test.values = processor.Counters[test.Group].Values
-
-	test.expect("lorem", 3)
-	test.expect("meh", 2)
-	test.expect("bacon", 4)
-	test.expect("foo", 0)
-	test.expect("bar", 0)
-}
 
 func TestCountProcessorForLevel(t *testing.T) {
 	entry := func(level string) *domain.Entry {
 		return &domain.Entry{Level: level}
 	}
-	test := CountTest{
+	countTest := test.CountTest{
 		Group:    "level",
 		NewEntry: entry,
 		T:        t,
 	}
-	test.doTest()
+	countTest.DoTest()
 }
 
 func TestCountProcessorForCategory(t *testing.T) {
 	entry := func(category string) *domain.Entry {
 		return &domain.Entry{Category: category}
 	}
-	test := CountTest{
+	countTest := test.CountTest{
 		Group:    "category",
 		NewEntry: entry,
 		T:        t,
 	}
-	test.doTest()
+	countTest.DoTest()
 }
 
 func TestCountProcessorForOrigin(t *testing.T) {
 	entry := func(origin string) *domain.Entry {
 		return &domain.Entry{Origin: origin}
 	}
-	test := CountTest{
+	countTest := test.CountTest{
 		Group:    "origin",
 		NewEntry: entry,
 		T:        t,
 	}
-	test.doTest()
+	countTest.DoTest()
 }
 
 func TestCountProcessorForException(t *testing.T) {
@@ -89,10 +49,10 @@ func TestCountProcessorForException(t *testing.T) {
 		e.AddException(exception)
 		return &e
 	}
-	test := CountTest{
+	countTest := test.CountTest{
 		Group:    "exception",
 		NewEntry: entry,
 		T:        t,
 	}
-	test.doTest()
+	countTest.DoTest()
 }
--- test/main.go
+++ test/main.go
@@ -4,8 +4,12 @@ import (
 	"bufio"
 	"strings"
 
+	"bytes"
+	"testing"
+
 	"github.com/devnull-tools/sherlog-holmes/domain"
 	"github.com/devnull-tools/sherlog-holmes/mappers"
+	"github.com/devnull-tools/sherlog-holmes/processors"
 )
 
 type MapperTest struct {
@@ -26,4 +30,42 @@ func (mapperTest *MapperTest) Test(lines string) chan *domain.Entry {
 		close(lineChan)
 	}()
 	return entryChan
+}
+
+type CountTest struct {
+	T        *testing.T
+	Group    string
+	NewEntry func(attr string) *domain.Entry
+	values   map[string]int64
+}
+
+func (test *CountTest) expect(label string, count int64) {
+	if n := test.values[label]; n != count {
+		test.T.Errorf("Invalid count for %s. Expected: %d | Got: %d", label, count, n)
+	}
+}
+
+func (test *CountTest) DoTest() {
+	var buffer bytes.Buffer
+	processor := processors.NewCountProcessor([]string{test.Group}, processors.Default, &buffer)
+
+	processor.Before()
+	processor.Execute(test.NewEntry("lorem"))
+	processor.Execute(test.NewEntry("bacon"))
+	processor.Execute(test.NewEntry("bacon"))
+	processor.Execute(test.NewEntry("lorem"))
+	processor.Execute(test.NewEntry("meh"))
+	processor.Execute(test.NewEntry("bacon"))
+	processor.Execute(test.NewEntry("meh"))
+	processor.Execute(test.NewEntry("lorem"))
+	processor.Execute(test.NewEntry("bacon"))
+	processor.After()
+
+	test.values = processor.Counters[test.Group].Values
+
+	test.expect("lorem", 3)
+	test.expect("meh", 2)
+	test.expect("bacon", 4)
+	test.expect("foo", 0)
+	test.expect("bar", 0)
 }
