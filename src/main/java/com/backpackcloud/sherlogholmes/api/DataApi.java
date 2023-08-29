6d0910f6f1f36eb314c90f495a2e54b46e5e52aa
Marcelo "Ataxexe" Guimarães <ataxexe@backpackcloud.com>
2023-08-29 04:13:02 +0200

    do it right next time, fucker

--- src/main/java/com/backpackcloud/sherlogholmes/api/DataApi.java
+++ src/main/java/com/backpackcloud/sherlogholmes/api/DataApi.java
@@ -2,6 +2,7 @@ package com.backpackcloud.sherlogholmes.api;
 
 import com.backpackcloud.cli.preferences.UserPreferences;
 import com.backpackcloud.sherlogholmes.Preferences;
+import com.backpackcloud.sherlogholmes.domain.AttributeType;
 import com.backpackcloud.sherlogholmes.domain.DataFilter;
 import com.backpackcloud.sherlogholmes.domain.DataRegistry;
 import com.backpackcloud.sherlogholmes.domain.FilterFactory;
@@ -11,10 +12,16 @@ import com.backpackcloud.sherlogholmes.impl.WebChart;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.Response;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 @Path("/api/data")
@@ -31,14 +38,13 @@ public class DataApi {
   }
 
   @GET
+  @Path("/chart")
   public Response getData(@QueryParam("label") String label,
                           @QueryParam("series") String series,
                           @QueryParam("reduction") String reduction,
                           @QueryParam("filter") String filter) {
     ChartBuilder builder = ChartBuilder.create(label, series, reduction);
-    DataFilter dataFilter = Arrays.stream(filter != null ? filter.split(",") : new String[0])
-      .map(filterFactory::create)
-      .reduce(DataFilter.ALLOW_ALL, DataFilter::and);
+    DataFilter dataFilter = createFilter(filter);
 
     Chart chart = builder.build(registry.entries().stream().filter(dataFilter));
     WebChart webChart = new WebChart(
@@ -48,6 +54,45 @@ public class DataApi {
     );
 
     return Response.ok(webChart).build();
+  }
+
+  @GET
+  @Path("/entries")
+  public Response entries(@QueryParam("fields") String fields, String filter) {
+    List<Map<String, Object>> result = new ArrayList<>();
+    DataFilter dataFilter = createFilter(filter);
+    String[] attributes = fields.split(",");
+
+    registry.entries().stream()
+      .filter(dataFilter)
+      .forEach(entry -> {
+        Map<String, Object> map = new HashMap<>();
+        Arrays.stream(attributes)
+          .forEach(attr -> map.put(attr, entry.valueOf(attr)));
+        result.add(map);
+      });
+
+    return Response.ok(result).build();
+  }
+
+  @GET
+  @Path("/index/{key}")
+  public Response index(@PathParam("key") String key) {
+    AttributeType type = registry.typeOf(key).orElse(AttributeType.TEXT);
+
+    List<String> result = registry.valuesFor(key)
+      .stream()
+      .map(value -> type.format(value))
+      .collect(Collectors.toList());
+
+    return Response.ok(result).build();
+  }
+
+  private DataFilter createFilter(String filter) {
+    DataFilter dataFilter = Arrays.stream(filter != null ? filter.split(",") : new String[0])
+      .map(filterFactory::create)
+      .reduce(DataFilter.ALLOW_ALL, DataFilter::and);
+    return dataFilter;
   }
 
 }
